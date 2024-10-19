7f0a8eae6256c186a62c7b605f17cceb6c405c41
GabrielFNM <gnovillo5437@utm.edu.ec>
2024-10-19 11:51:18 -0500

    third commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, flash, redirect, render_template, request, url_for
 from flask_mysqldb import MySQL
 app = Flask(__name__)
 
@@ -9,18 +9,28 @@ app.config['MYSQL_DB'] = 'MCPROJECT'
 
 mysqldb = MySQL(app)
 
+#setting
+app.secret_key='mysecretkey'
+
 @app.route('/')
 def Index():
-    return render_template('index.html')
+    cursor2 = mysqldb.connection.cursor()
+    cursor2.execute('SELECT * FROM TABLE1')
+    data = cursor2.fetchall()
+    return render_template('index.html', users = data)
 
 
 @app.route('/add_record', methods=['POST'])
 def add():
     if request.method == 'POST':
-        dato1 = request.form['input1']
+        names = request.form['nombres']
+        lastnames = request.form['apellidos']
+        ages = request.form['edad']
         cursor =  mysqldb.connection.cursor()
-        cursor.execute('9NSERT INTO SOMETABLE (dato1) VALUES (%s)',(dato1,))
-        return 'recieved'
+        cursor.execute('INSERT INTO TABLE1 (nombres,apellidos,edad) VALUES (%s,%s,%s)',(names,lastnames,ages))
+        mysqldb.connection.commit()
+        flash('User added Succesfully')
+        return redirect(url_for('index.html'))
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
