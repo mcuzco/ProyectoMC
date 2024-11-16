64a954532b2158822e72475b985da387ae55e523
Ponce-Rafael <rponce7092@utm.edu.ec>
2024-11-16 18:29:37 -0500

    BD

--- sql/db.sql
+++ sql/db.sql
@@ -93,5 +93,12 @@ CREATE TABLE historial_reservas (
     FOREIGN KEY (reserva_id) REFERENCES reservas(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+-- Tabla para registrar usuarios
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
 ALTER TABLE habitaciones
 ADD COLUMN estado ENUM('ocupada', 'desocupada') DEFAULT 'desocupada';
